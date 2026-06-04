8d1583a818b27e84d64492439b06f80b86193670
ansh-verma-07 <rewavrema@gmail.com>
2026-06-04 11:21:52 +0530

    Update index.ts

--- public/index.ts
+++ public/index.ts
@@ -2,24 +2,5 @@
 export { default as eyes } from './svg/eyes.svg';
 export { default as mask } from './svg/mask.svg';
 
-
-// projects lists item
-
-export { default as alkemy } from './projects/alkemy.png';
-export { default as launchpad } from './projects/launchpad.png';
-export { default as ainewsaggregator } from './projects/ai-news-aggregator.png';
-export {default as dental_learning} from './projects/dental-learning.png';
-export {default as vibehack} from './projects/vibe-hack.png';
-export {default as skillxchange} from './projects/skillXchnage.png';
-
-// education & experience
-export { default as gyanGanga } from './education/gyanGanga.jpeg';
-export { default as bharati } from './education/bharati.jpg';
-
-export { default as devnovate } from './experience/devnovate.jpg';
-export { default as hwi} from './experience/hwi.png'
-export { default as crdscr} from './experience/centralrailway.png'
-export { default as alkemy_2025} from './experience/alkemy.png'
-
 // skills page assets
 export {default as JWt } from './svg/Jwt-Icon--Streamline-Svg-Logos.svg';
