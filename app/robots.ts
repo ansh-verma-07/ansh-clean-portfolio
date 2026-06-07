69ea41d28eb7f0b4dc917f9b80bd153582db1eb2
ansh-verma-07 <rewavrema@gmail.com>
2026-06-07 11:56:07 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
 			allow: '/',
 			disallow: '/api/',
 		},
-		sitemap: 'https://ansh-clean-portfolio.vercel.app/sitemap.xml',
+		sitemap: 'https://www.ansh-verma.xyz/sitemap.xml',
 	};
 }
